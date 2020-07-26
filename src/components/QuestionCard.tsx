c2e309ff5ac248befd6f484ef0b5892d741b5877
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-07-27 10:54:52 +1200

    implement QuestionCard to show questions and answers

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,14 +1,39 @@
 import React from "react";
 
 type Props = {
-    question: string;
-    answer: string[];
-    callback: any;
-    userAnswer: string;
-    questionNr: number;
-    totalQuestions: number;
+  question: string;
+  answers: string[];
+  callback: any;
+  userAnswer: boolean;
+  questionNr: number;
+  totalQuestions: number;
 };
 
-const QuestionCard = () => <div>Question Card</div>;
+//FC tells React that this is a functional component
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNr,
+  totalQuestions,
+}) => (
+  <div>
+    <p className="number">
+      Question: {questionNr} / {totalQuestions}
+    </p>
+    {/*renders some HTML*/}
+    <p dangerouslySetInnerHTML={{ __html: question }} />
+    <div>
+      {answers.map((answer) => (
+        <div>
+            <button disabled={userAnswer} onClick={callback}>
+                <span dangerouslySetInnerHTML={{__html: answer}} />
+            </button>
+        </div>
+      ))}
+    </div>
+  </div>
+);
 
 export default QuestionCard;
