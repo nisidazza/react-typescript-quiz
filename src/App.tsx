ee695c24e913a6e306dbbeb41677f537c838f22d
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-07-27 10:34:10 +1200

    add some buttons and paragraphs to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+//Components
+import QuestionCard from './components/QuestionCard';
 
 
 const App = () => {
@@ -7,16 +9,24 @@ const App = () => {
 
   }
 
+  //sent in by props to the QuestionCard component
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
 
   }
 
   const nextQuestion = () => {
-    
+
   }
 
   return (
-    <div className="App">Quiz</div>
+    <div className="App">
+      <h1>Trivia Quiz</h1>
+      <button className='start' onClick={startTrivia}>Start</button>
+      <p className='score'>Score:</p>
+      <p>Loading Questions...</p>
+      <QuestionCard/>
+      <button className='next' onClick={nextQuestion}>Next Question</button>
+      </div>
   );
 }
 
