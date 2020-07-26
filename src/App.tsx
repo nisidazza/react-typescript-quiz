b698a8d0bf21346fb860b1699a1c034507085b1f
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-07-27 10:17:38 +1200

    create functions to start the game, check the user's answer and go to the next question

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
 
-function App() {
+const App = () => {
+
+  const startTrivia = async () => {
+
+  }
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const nextQuestion = () => {
+    
+  }
+
   return (
     <div className="App">Quiz</div>
   );
